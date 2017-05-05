b123f08f941f2f744f04b592dcdb34ab3016ab09
Akash <akashathale@gmail.com>
2017-05-05 01:35:10 -0400

    day added

---
+++ class_to_collect.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <ctime>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+class gather_info{
+        public:
+        long int ret_long(int j);
+        string ret_str(int j);
+        int ret_int();
+};
+
+void yesterday(){
+	cout<<"Yesterday was a Monday"<<endl;
+}
